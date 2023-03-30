6aca96f64a531b3d7942f69a57015376b48ec0fe
Lara Herzog <lara@qbaylogic.com>
2023-03-30 11:21:56 +0200

    add DBusCachedPlugin

--- vex-riscv/example-cpu/src/main/scala/example/ExampleCpu.scala
+++ vex-riscv/example-cpu/src/main/scala/example/ExampleCpu.scala
@@ -8,6 +8,7 @@ import spinal.core._
 import spinal.lib._
 import vexriscv.plugin._
 import vexriscv.{VexRiscv, VexRiscvConfig, plugin}
+import vexriscv.ip.{DataCacheConfig}
 
 object ExampleCpu extends App {
   def cpu() : VexRiscv = {
@@ -21,9 +22,36 @@ object ExampleCpu extends App {
           catchAccessFault = true,
           compressedGen = true // C extension
         ),
+        /*
         new DBusSimplePlugin(
           catchAddressMisaligned = true,
           catchAccessFault = true
+        ),
+        */
+        new DBusCachedPlugin(
+          dBusCmdMasterPipe = true,
+          dBusCmdSlavePipe = true,
+          dBusRspSlavePipe = false,
+          relaxedMemoryTranslationRegister = false,
+          config = new DataCacheConfig(
+            cacheSize = 4096,
+            bytePerLine = 32,
+            wayCount = 1,
+            addressWidth = 32,
+            cpuDataWidth = 32,
+            memDataWidth = 32,
+            catchAccessError = true,
+            catchIllegal = true,
+            catchUnaligned = true,
+            withLrSc = true,
+            withAmo = true,
+            earlyWaysHits = true
+          ),
+          memoryTranslatorPortConfig = MmuPortConfig(portTlbSize = 4),
+          csrInfo = true
+        ),
+        new StaticMemoryTranslatorPlugin(
+          ioRange = (x => x(31 downto 24) === 0x0)
         ),
         new CsrPlugin(
           CsrPluginConfig.smallest.copy(ebreakGen = true, mtvecAccess = CsrAccess.READ_WRITE)
@@ -70,7 +98,15 @@ object ExampleCpu extends App {
           plugin.iBus.setAsDirectionLess() //Unset IO properties of iBus
           master(plugin.iBus.toWishbone()).setName("iBusWishbone")
         }
+        case plugin: IBusCachedPlugin => {
+          plugin.iBus.setAsDirectionLess() //Unset IO properties of iBus
+          master(plugin.iBus.toWishbone()).setName("iBusWishbone")
+        }
         case plugin: DBusSimplePlugin => {
+          plugin.dBus.setAsDirectionLess()
+          master(plugin.dBus.toWishbone()).setName("dBusWishbone")
+        }
+        case plugin: DBusCachedPlugin => {
           plugin.dBus.setAsDirectionLess()
           master(plugin.dBus.toWishbone()).setName("dBusWishbone")
         }
